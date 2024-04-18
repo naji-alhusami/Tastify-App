8103b0c633d539c23d6b5f6ed36ad95ea6e97dd8
naji-alhusami <najihussami@gmail.com>
2024-04-18 13:31:53 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-5xl bg-green-300">Starting Project</div>
+      <div className="text-2xl bg-green-300">Starting Project</div>
+    </>
+  );
+}
+
+export default App;
